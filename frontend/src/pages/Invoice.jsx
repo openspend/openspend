e7369055970fcc16efd219b0434b6b410118101f
Umair Ashraf <umrashrf@gmail.com>
2026-06-14 10:29:17 -0400

    Show security question and answer

--- frontend/src/pages/Invoice.jsx
+++ frontend/src/pages/Invoice.jsx
@@ -3,6 +3,7 @@ import { useLocation, useRoute } from "preact-iso";
 import QRCode from 'easyqrcodejs';
 import { db } from '../postbase';
 import { Timestamp } from '../../lib/postbase/db';
+import { MdCopyAll } from 'react-icons/md';
 
 const TAXES = [
     {
@@ -136,6 +137,19 @@ export function Invoice() {
         }, 500);
     };
 
+    const copyToClipboard = async (event, textToCopy) => {
+        try {
+            const target = event.currentTarget || event.target;
+            await navigator.clipboard.writeText(textToCopy);
+            target.innerText = '✅';
+            setTimeout(
+                () => target.innerHTML = '<svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" height="24" width="24" xmlns="http://www.w3.org/2000/svg"><path fill="none" d="M0 0h24v24H0z"></path><path d="M18 2H9c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h9c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zm0 14H9V4h9v12zM3 15v-2h2v2H3zm0-5.5h2v2H3v-2zM10 20h2v2h-2v-2zm-7-1.5v-2h2v2H3zM5 22c-1.1 0-2-.9-2-2h2v2zm3.5 0h-2v-2h2v2zm5 0v-2h2c0 1.1-.9 2-2 2zM5 6v2H3c0-1.1.9-2 2-2z"></path></svg>',
+                2000);
+        } catch (err) {
+            console.error('Failed to copy!', err);
+        }
+    };
+
     const markAsPaid = async (event) => {
         if (confirm("Are you sure?")) {
             await db.collection('invoices').doc(invoice.id).set({
@@ -180,8 +194,29 @@ export function Invoice() {
                         <p class="text-xl">Status: <span class="text-red-600">Draft (Unpaid)</span></p>
                     </div>
 
+                    <div class="mt-6 text-xl">
+                        <div class="grid grid-cols-2 gap-2 relative">
+                            <p class="text-right">Security Question:</p>
+                            <p class="text-left font-bold">Code</p>
+                            <button class="absolute top-0 bottom-0 right-0 hover:bg-gray-300 rounded"
+                                title="Copy Question"
+                                onClick={e => copyToClipboard(e, "Code")}>
+                                <MdCopyAll size={24} />
+                            </button>
+                        </div>
+                        <div class="grid grid-cols-2 gap-2 relative">
+                            <p class="text-right">Security Answer:</p>
+                            <p class="text-left font-bold">{invoice?.uniqueIdentifier}</p>
+                            <button class="absolute top-0 bottom-0 right-0 hover:bg-gray-300 rounded"
+                                title="Copy Answer"
+                                onClick={e => copyToClipboard(e, invoice?.uniqueIdentifier)}>
+                                <MdCopyAll size={24} />
+                            </button>
+                        </div>
+                    </div>
+
                     <div class="mt-8">
-                        {showQrCode && <button class="flex bg-white items-center justify-center"
+                        {showQrCode && <button class="w-full flex items-center justify-center"
                             onClick={e => setShowQrCode(false)}>
                             <div>
                                 <div ref={qrCodeRef}></div>
