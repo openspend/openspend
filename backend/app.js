1c58467209f9c2a38ff62a91b43788cdf2ea9e8b
Umair Ashraf <umrashrf@gmail.com>
2026-06-14 23:15:29 -0400

    Allow HTTP OPTIONS in CORS

--- backend/app.js
+++ backend/app.js
@@ -59,7 +59,7 @@ router.use('/email', emailRouter);
 // For local testing
 // app.use(cors({
 //     origin: ["http://localhost:5173", "http://localhost:5174"],
-//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     methods: ["OPTIONS", "GET", "POST", "PUT", "DELETE"],
 //     credentials: true,
 // }));
 app.use(express.json());
