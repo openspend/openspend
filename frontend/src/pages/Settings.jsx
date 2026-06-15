96a2d734517c97f475861c6c60b14d03f86b7d73
Umair Ashraf <umrashrf@gmail.com>
2026-06-14 20:38:17 -0400

    Finish settings page

--- frontend/src/pages/Settings.jsx
+++ frontend/src/pages/Settings.jsx
@@ -1,39 +1,195 @@
 import { useEffect, useState } from 'preact/hooks';
+import { useLocation } from 'preact-iso';
+import { IoClose } from 'react-icons/io5';
+import { getAuth } from '../auth';
+import { db } from '../postbase';
 
 export function Settings() {
-    const [code, setCode] = useState("")
-    const [email, setEmail] = useState("");
-    const [currency, setCurrency] = useState("CAD");
-    const [currencySymbol, setCurrencySymobol] = useState("$");
-    const [amount, setAmount] = useState(0.01);
+    const location = useLocation();
+    const [loading, setLoading] = useState(false);
+    const [user, setUser] = useState(null);
+    const [brand, setBrand] = useState(
+        // {
+        //     name: 'CasaZero',
+        //     depositEmail: 'dasvilleda@gmail.com',
+        //     currency: 'CAD',
+        //     currencySymbol: '$',
+        //     uniqueIdentifier: true,
+        //     taxes: [{
+        //         name: 'TVQ',
+        //         value: '9.975%',
+        //         percent: 9.975 / 100,
+        //     },
+        //     {
+        //         name: 'TPS',
+        //         value: '5%',
+        //         percent: 5 / 100,
+        //     }],
+        // }
+    );
 
-    const search = async (event) => {
-        event.preventDefault();
+    useEffect(() => {
+        setLoading(false);
 
-        const uniqueIdentifier = generateSixDigitAlphanumeric();
-        const formData = {
-            email: `DASVILLEDA+${uniqueIdentifier}@GMAIL.COM`,
-            currency,
-            currencySymbol,
-            amount,
-            uniqueIdentifier,
+        const auth = getAuth();
+
+        if (!user) {
+            setUser(auth.currentUser);
         }
 
-        const resp = await fetch(import.meta.env.VITE_API_BASE + '/invoice', {
-            method: 'POST',
-            body: JSON.stringify(formData),
+        const unsubscribe = auth.onAuthStateChanged(async (user) => {
+            setLoading(false);
+            if (user) {
+                setUser(user);
+            } else {
+                const redirectUrl = window.location.href.replace(window.location.origin, '');
+                location.route('/login?redirectUrl=' + redirectUrl);
+            }
         });
 
-        const data = await resp.json();
-        const invoiceId = data.id;
+        return () => unsubscribe();
+    }, []);
 
-        // generate qr code
+    useEffect(() => {
+        if (!user) return;
+
+        (async () => {
+            const brandDoc = await db.collection('brands').doc(user.id).get();
+            setBrand({
+                id: brandDoc.id,
+                ...brandDoc.data(),
+            });
+        })();
+
+    }, [user]);
+
+    const handleChange = (event) => {
+        const target = event.currentTarget || event.target;
+        const { name, value } = target;
+        setBrand({ ...brand, [name]: value });
+    }
+
+    const update = async (event) => {
+        event?.preventDefault();
+
+        const formData = new FormData(event.target);
+        const data = Object.fromEntries(formData.entries());
+
+        await db.collection('brands').doc(user.id).set(data, { merge: true });
     };
+
+    const addTax = async (event) => {
+        event?.preventDefault();
+
+        const formData = new FormData(event.target);
+        const data = Object.fromEntries(formData.entries());
+
+        let taxes = [];
+        if (brand && brand?.taxes) {
+            taxes = brand.taxes;
+        }
+
+        taxes.push(
+            {
+                name: data.name,
+                value: data.value,
+                percent: parseFloat(data.percent),
+            },
+        );
+
+        console.log(taxes);
+
+        await db.collection('brands').doc(user.id).set({ taxes }, { merge: true });
+
+        const brandDoc = await db.collection('brands').doc(user.id).get();
+        setBrand({
+            id: brandDoc.id,
+            ...brandDoc.data(),
+        });
+    };
+
+    if (loading) {
+        return <div class="loading">Loading...</div>;
+    }
 
     return (
         <div class="w-full flex flex-col items-center">
-            <p class="mt-4 mb-10 text-4xl text-center">Settings</p>
-            <p>TO-DO</p>
+            <h2 class="mt-4 mb-10 text-4xl text-center">Settings</h2>
+
+            <div class="w-full md:w-150 p-2">
+                <div class="">
+                    <h3 class="mt-4 mb-10 text-3xl text-center">Brand / Company Name</h3>
+                    <form class="w-full grid grid-cols-3 gap-2" onSubmit={update}>
+                        <input name="name" placeholder="Brand or Company Name" value={brand?.name}
+                            class="col-span-2 p-4 border-1" onChange={handleChange} />
+                        <button type="submit" class="bg-blue-600 text-white p-4 rounded cursor-pointer">Update</button>
+                    </form>
+                </div>
+                <p class="text-sm">This is the contact name customers will use to save your email in their online banking app.</p>
+            </div>
+
+            <div class="w-full md:w-150 p-2">
+                <h3 class="mt-4 mb-10 text-3xl text-center">Email for Deposit</h3>
+                <form class="flex flex-col gap-2" onSubmit={update}>
+                    <div class="w-full grid grid-cols-3 gap-2">
+                        <input name="depositEmail" placeholder="Email for online banking deposit (Interac for Canada)"
+                            value={brand?.depositEmail}
+                            class="col-span-2 p-4 border-1" />
+                        <button type="submit" class="bg-blue-600 text-white p-4 rounded cursor-pointer">Update</button>
+                    </div>
+                    <label>
+                        <p class="text-sm">
+                            <input name="uniqueIdentifier" type="checkbox"
+                                checked={brand?.uniqueIdentifier ?? true} disabled /> Add unique identifier to email for each transaction (Enabled by default)
+                        </p>
+                        <p class="ml-5 text-xs">
+                            OpenSpend adds <i>+unique_identifier</i> suffix to your email part before @ to uniquely identify each transaction.
+                        </p>
+                    </label>
+                </form>
+            </div>
+
+            <div class="w-full md:w-150 p-2">
+                <h3 class="mt-4 mb-10 text-3xl text-center">Currency</h3>
+                <form class="flex flex-col gap-2" onSubmit={update}>
+                    <p>Currency Symbol ($, €, etc)</p>
+                    <input name="currencySymbol" placeholder="$" value={brand?.currencySymbol}
+                        class="col-span-2 p-4 border-1" />
+                    <p>Currency (USD, EUR, GBP, CAD, AUD)</p>
+                    <input name="currency" placeholder="USD, EUR, GBP, CAD, AUD" value={brand?.currency}
+                        class="col-span-2 p-4 border-1" />
+                    <button type="submit" class="bg-blue-600 text-white p-4 rounded cursor-pointer">Update</button>
+                </form>
+            </div>
+
+            <div class="p-2">
+                <h3 class="mt-4 mb-10 text-3xl text-center">Taxes</h3>
+
+                <div class="grid grid-cols-3 gap-2 border-b-1 mb-2">
+                    <p>Tax Name</p>
+                    <p>Tax Title</p>
+                    <p>Tax Percent</p>
+                </div>
+                {brand && brand?.taxes && brand?.taxes.map(t => <div class="relative grid grid-cols-3 gap-2 flex items-center mt-2">
+                    <p>{t.name}</p>
+                    <p>{t.value}</p>
+                    <p>{t.percent.toFixed(5)}</p>
+                    <button class="absolute right-1 cursor-pointer">
+                        <IoClose />
+                    </button>
+                </div>)}
+                <form class="flex flex-col gap-2 mt-4" onSubmit={addTax}>
+                    <div class="grid grid-cols-3 gap-2">
+                        <input name="name" placeholder="Name (Provincial or QST)"
+                            class="p-4 border-1" />
+                        <input name="value" placeholder="Title (9.975%)"
+                            class="p-4 border-1" />
+                        <input name="percent" placeholder="Percent (0.09975)"
+                            class="p-4 border-1" />
+                    </div>
+                    <button type="submit" class="bg-blue-600 text-white p-4 rounded cursor-pointer">Add</button>
+                </form>
+            </div>
         </div>
     );
 }
