d7478a24656fd153aca868fa2804a0b768587b67
Umair Ashraf <umrashrf@gmail.com>
2026-06-14 23:33:29 -0400

    Delete unused pages

--- frontpage/src/App.jsx
+++ frontpage/src/App.jsx
@@ -2,13 +2,7 @@ import { useEffect, useState } from 'preact/hooks';
 import { LocationProvider, Router, Route } from 'preact-iso';
 import Header from './components/Header';
 import Home from './pages/Home';
-import Dashboard from './pages/Dashboard';
-import Login from './pages/Login';
 import { getSession } from './auth';
-import { Invoice } from './pages/Invoice';
-import { Pay } from './pages/Pay';
-import { Search } from './pages/Search';
-import { Settings } from './pages/Settings';
 
 export default function App() {
     const [user, setUser] = useState(null);
@@ -29,13 +23,6 @@ export default function App() {
             <LocationProvider>
                 <Router>
                     <Route path="/" component={Home} user={user} />
-                    <Route path="/dashboard" component={Dashboard} user={user} />
-                    <Route path="/login" component={Login} user={user} />
-                    <Route path="/invoice" component={Invoice} user={user} />
-                    <Route path="/invoice/:invoiceId" component={Invoice} user={user} />
-                    <Route path="/pay/:invoiceId" component={Pay} user={user} />
-                    <Route path="/search" component={Search} user={user} />
-                    <Route path="/settings" component={Settings} user={user} />
                 </Router>
             </LocationProvider>
 
