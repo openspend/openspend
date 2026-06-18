78f50246a12afc58a167dcd98dd795240d24f9e3
Umair Ashraf <umrashrf@gmail.com>
2026-06-18 10:48:06 -0400

    New users get $5

--- frontpage/src/pages/Home.jsx
+++ frontpage/src/pages/Home.jsx
@@ -19,6 +19,7 @@ export default function Home({ user }) {
                         <div className="mt-8 flex flex-col gap-2">
                             <a href="https://app.openspend.riamu.io" className="w-max bg-blue-600 text-4xl text-white px-6 py-3 rounded-md font-medium shadow hover:bg-blue-700 transition">Try for free</a>
                             <p class="text-3xl">No credit card required</p>
+                            <p class="text-xl">New users get $5</p>
                         </div>
                     </div>
 
