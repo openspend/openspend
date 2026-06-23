7f1a5f9b42ac80a7d0bf40d4615c7c0171086651
Umair Ashraf <umrashrf@gmail.com>
2026-06-23 14:57:43 -0400

    $600 Credit

--- frontpage/src/components/Header.jsx
+++ frontpage/src/components/Header.jsx
@@ -12,7 +12,12 @@ export default function Header({ user }) {
     }, []);
 
     return (
-        <header className={`w-full z-40 transition-all ${scrolled ? 'bg-white shadow' : 'bg-transparent'} backdrop-blur`}>
+        <header className={`w-full z-40 rounded-none transition-all ${scrolled ? 'bg-white shadow' : 'bg-transparent'} backdrop-blur`}>
+            <div class="flex flex-col md:flex-row items-center justify-center gap-1 text-center text-sm font-bold bg-green-200 text-green-700 px-2 py-1">
+                <p>$600 Credit</p>
+                <p>when you spend $600 within 60 days</p>
+            </div>
+
             <div className="flex flex-col md:flex-row items-center gap-1 max-w-6xl mx-auto px-6 py-4 flex justify-between items-center">
                 <a href="/" className="text-2xl font-extrabold text-blue-600 mb-4 md:mb-0">OpenSpend</a>
                 <nav className="grid grid-cols-2 gap-2 text-center md:flex md:flex-row md:gap-8 text-lg font-medium items-center justify-center">
